40a9df7511dc1d17e03e4aaeb27b01a5eaf316da
Matthias Mulugeta <matthias22m@gmail.com>
2024-06-19 17:36:40 +0300

    styles: removed comment

--- core/models.py
+++ core/models.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(unique=True,max_length=255)
