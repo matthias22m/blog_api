dc40011d5405998e32231bf996a93dd4cbcb14c3
Matthias Mulugeta <matthias22m@gmail.com>
2024-06-19 17:46:38 +0300

    chore: change jwt urls

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, PostViewSet, CommentViewSet, LikedItemViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
@@ -14,6 +11,4 @@ router.register(r'liked_items', LikedItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
